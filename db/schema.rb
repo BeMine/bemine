616b44dfd26f9c75de92e49d34c0aa4fbf78f0fd
Brian Tai <alphabt@gmail.com>
2016-01-11 17:25:33 +0800

    Auto generate db schema

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160107094042) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "categories", force: :cascade do |t|
     t.string   "name"
     t.text     "description"
@@ -51,10 +54,7 @@ ActiveRecord::Schema.define(version: 20160107094042) do
     t.string   "matcharea"
   end
 
-  add_index "products", ["category_id"], name: "index_products_on_category_id"
-
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  add_index "products", ["category_id"], name: "index_products_on_category_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -76,4 +76,5 @@ ActiveRecord::Schema.define(version: 20160107094042) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["fb_uid"], name: "index_users_on_fb_uid", using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+
 end
