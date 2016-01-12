ba0b902ebda8b20386ff17cf9c0170f8b3f56e44
stanleykuooo <zowie7912@hotmail.com>
2016-01-12 14:12:41 +0800

    welcome and css

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'welcome#index'
+  root 'welcomes#index'
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
    resources :categories do
