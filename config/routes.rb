874101a740775ca5bd9ec19430f68a5d7334d4f0
Brian Tai <alphabt@gmail.com>
2016-01-18 15:36:44 +0800

    Install letter_opener_web

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,10 @@ Rails.application.routes.draw do
   end
 
   resources :orders
+
+  Bemine::Application.routes.draw do
+    mount LetterOpenerWeb::Engine, at: '/letter_opener'
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
