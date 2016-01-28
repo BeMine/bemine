49276c05b8367d171cafd21b75b0f5e6f2d4224e
Brian Tai <alphabt@gmail.com>
2016-01-29 04:19:02 +0800

    Add role to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160128072054) do
+ActiveRecord::Schema.define(version: 20160128185243) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -129,6 +129,7 @@ ActiveRecord::Schema.define(version: 20160128072054) do
     t.string   "fb_token"
     t.string   "name"
     t.string   "payment_info"
+    t.string   "role"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
