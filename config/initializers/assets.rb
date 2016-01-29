01e901ec851c92d86ad198ba7a4047c3643981e0
Brian Tai <alphabt@gmail.com>
2016-01-29 11:24:20 +0800

    Load controller specific stylesheet

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,4 +11,4 @@ end
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w( carts.css categories.css emails.css orders.css products.css welcomes.css )
+Rails.application.config.assets.precompile += %w(emails.css welcomes.css)
