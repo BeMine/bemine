707354fd5318a94d4ce8c7d0b8ba55f987a6c293
Brian Tai <alphabt@gmail.com>
2016-01-29 10:46:05 +0800

    Make token unique in fulfil requests

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160128185243) do
+ActiveRecord::Schema.define(version: 20160129024104) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,7 +61,7 @@ ActiveRecord::Schema.define(version: 20160128185243) do
   end
 
   add_index "fulfill_requests", ["order_id"], name: "index_fulfill_requests_on_order_id", using: :btree
-  add_index "fulfill_requests", ["token"], name: "index_fulfill_requests_on_token", using: :btree
+  add_index "fulfill_requests", ["token"], name: "index_fulfill_requests_on_token", unique: true, using: :btree
   add_index "fulfill_requests", ["user_id"], name: "index_fulfill_requests_on_user_id", using: :btree
 
   create_table "line_items", force: :cascade do |t|
